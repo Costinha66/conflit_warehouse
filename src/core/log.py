542d90db36625f8478514db52635c6adcd1d7761
costinha66 <filipe-edge@hotmail.com>
2025-08-26 14:44:55 +0200

    Setup enviroment

---
+++ src/core/log.py
@@ -0,0 +1,35 @@
+# File to configure logging
+
+import logging
+import structlog
+
+
+def configure_logging(json_logs: bool = True):
+    timestamper = structlog.processors.TimeStamper(fmt="iso")
+
+    pre_chain = [
+        timestamper,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.add_logger_name,
+    ]
+
+    if json_logs:
+        renderer = structlog.processors.JSONRenderer()
+    else:
+        renderer = structlog.dev.ConsoleRenderer(colors=True)
+
+    structlog.configure(
+        processors=[
+            structlog.stdlib.filter_by_level,
+            *pre_chain,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            renderer,
+        ],
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+
+    logging.basicConfig(level=logging.INFO)
