9836836eea257b7585f134bacd2243499aa158de
costinha66 <filipe-edge@hotmail.com>
2025-09-08 16:00:43 +0200

    Discovery

--- src/silver/upsert_cli.py
+++ src/silver/upsert_cli.py
@@ -1,22 +1,8 @@
-# src/silver/unhcr_upsert_cli.py
-import time
-import structlog
 from src.core.log import configure_logging
 
 
 def main(version: str):
     configure_logging(True)
-    log = structlog.get_logger()
-    s = time.time()
-    # TODO: upsert changes into silver table; run DQ checks
-    log.info(
-        "silver_upsert_done",
-        source="unhcr",
-        version=version,
-        records_out=0,
-        dq_passed=True,
-        duration_sec=round(time.time() - s, 3),
-    )
 
 
 if __name__ == "__main__":
